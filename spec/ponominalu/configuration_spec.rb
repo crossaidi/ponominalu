528a610c76570933e53d2af36f354ffde2a47bc1
Kovalevsky Dmitry <deecross33@gmail.com>
2014-05-11 01:02:31 +0400

    Finished configuration tests

--- spec/ponominalu/configuration_spec.rb
+++ spec/ponominalu/configuration_spec.rb
@@ -1,14 +1,30 @@
 require 'spec_helper'
 
 describe Ponominalu::Configuration do
-  subject { Ponominalu }
+
+  class ConfigurationTest
+    extend Ponominalu::Configuration
+  end
+
+  subject { ConfigurationTest }
+
   describe '.configure' do
     let(:options) { { session: '321', empty_strict: true, max_retries: 4 } }
+
     it 'configured by passed hash' do
       subject.configure(options)
       expect(subject.session).to eq('321')
       expect(subject.empty_strict).to be_true
       expect(subject.max_retries).to eq(4)
+    end
+
+    it 'yields self' do
+      expect(subject).to receive(:option)
+      subject.configure { |pn| pn.option }
+    end
+
+    it 'returns self' do
+      expect(subject.configure).to eq(subject)
     end
   end
 
@@ -17,5 +33,23 @@ describe Ponominalu::Configuration do
       .from(true).to(false)
     expect { subject.log_responses = true }.to change { subject.log_responses? }
       .from(false).to(true)
+  end
+
+  describe '.reset' do
+    it 'resets options to default values' do
+      subject.reset
+
+      expect(subject.adapter).to eq(:net_http)
+      expect(subject.logger).to be_instance_of(::Logger)
+      expect(subject.session).to eq('123')
+      expect(subject.max_retries).to eq(2)
+      expect(subject.empty_strict).to be_false
+      expect(subject.raw_json).to be_false
+      expect(subject.log_requests).to be_true
+      expect(subject.log_responses).to be_false
+      expect(subject.wrap_response).to be_false
+      expect(subject.http_verb).to eq(:post)
+      expect(subject.faraday_options).to eq({})
+    end
   end
 end
